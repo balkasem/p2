c0f9cf0bb390dcff59dc092450ec2626513d5d2d
Sam Balkasem <obalkasem@gmail.com>
2018-02-28 20:02:09 -0500

    modify and fix index.php

--- index.php
+++ index.php
@@ -10,7 +10,6 @@ require 'index-logic.php';
     <link href='css/main.css' rel='stylesheet' >
     <title>Bill Splitter</title>
     <meta charset='utf-8'>
-    <meta charset='utf-8'>
     <link href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'
           rel='stylesheet' integrity='sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm'
           crossorigin='anonymous'>
